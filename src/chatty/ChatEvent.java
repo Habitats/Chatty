80be602b45a02ebcdbc85c0fae32b3c6525c5ddc
anon <no@ty.com>
2013-02-12 19:58:52 +0100

    modified ChatEvent to suit the new object-based network com.

---
+++ src/chatty/ChatEvent.java
@@ -0,0 +1,71 @@
+package chatty;
+
+import java.io.Serializable;
+import java.util.Date;
+
+
+public class ChatEvent implements Serializable {
+
+	private final User from;
+	private final User to;
+	private final String msg;
+	private final String[] msgArr;
+
+	private final Date sendDate;
+	private Date receivedDate;
+
+	private ChatCommand cmd;
+
+	private String returnMsg;
+
+	public ChatEvent(String msg) {
+		this(null, null, msg);
+	}
+
+	public ChatEvent(User from, User to, String msg) {
+		this.from = from;
+		this.to = to;
+		this.msg = msg;
+		this.sendDate = new Date();
+		msgArr = msg.split(" ");
+		if (msgArr[0].startsWith("/")) {
+			cmd = ChatCommand.getCmd(msgArr[0]);
+		}
+	}
+
+	public boolean isCommand() {
+		return (cmd != null);
+	}
+
+	public ChatCommand getCommand() {
+		return cmd;
+	}
+
+	public User getFrom() {
+		return from;
+	}
+
+	public User getTo() {
+		return to;
+	}
+
+	public String[] getMsgArr() {
+		return msgArr;
+	}
+
+	public String getReturnMsg() {
+		return returnMsg;
+	}
+
+	public String getMsg() {
+		return msg;
+	}
+
+	public Date getReceivedDate() {
+		return receivedDate;
+	}
+
+	public void setReceivedDate() {
+		this.receivedDate = new Date();
+	}
+}
---
+++ src/chatty/User.java
@@ -0,0 +1,12 @@
+package chatty;
+
+public class User {
+	private final String name;
+
+	public User(String name) {
+		this.name = name;
+	}
+	public String getName() {
+		return name;
+	}
+}
